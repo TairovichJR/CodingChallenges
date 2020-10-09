9d345b67e3c326e962d4d41a8bc93d0b138c80ce
Omonjon Yokubov <yokubov.omonjon@gmail.com>
2020-10-09 05:39:36 -0400

    Adding coding bat medium array challenges

---
+++ src/codingbat/CodingBat_Arrays.java
@@ -0,0 +1,148 @@
+package codingbat;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * Created by tairovich_jr on Oct 09, 2020
+ */
+public class CodingBat_Arrays {
+
+	public int sum67(int[] nums) {
+		int sum = 0;
+		boolean flag = true;
+		// [1, 2, 6, 99, 99, 7]
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != 6) {
+				if (flag) {
+					sum += nums[i];
+				} else if (nums[i] == 7) {
+					flag = true;
+				}
+
+			} else {
+				flag = false;
+			}
+		}
+		return sum;
+	}
+
+	public boolean has22(int[] nums) {
+
+		if (nums.length < 2) {
+			return false;
+		}
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == 2 && nums[i + 1] == 2)
+				return true;
+
+		}
+		return false;
+	}
+
+	public boolean lucky13(int[] nums) {
+
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 1 || nums[i] == 3)
+				return false;
+		}
+		return true;
+	}
+
+	public boolean sum28(int[] nums) {
+		return 8 == (int) Arrays.stream(nums).filter(n -> n == 2).sum();
+	}
+
+	public boolean more14(int[] nums) {
+		int _1s = (int) Arrays.stream(nums).filter(n -> n == 1).count();
+		int _4s = (int) Arrays.stream(nums).filter(n -> n == 4).count();
+		return _1s > _4s ? true : false;
+	}
+
+	public int[] fizzArray(int n) {
+		int[] ints = new int[n];
+		for (int i = 0; i < ints.length; i++) {
+			ints[i] = i;
+		}
+		return ints;
+
+	}
+
+	public boolean only14(int[] nums) {
+
+		if (nums.length == 0) {
+			return true;
+		}
+
+		boolean flag = false;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 1 || nums[i] == 4) {
+				flag = true;
+			} else {
+				return false;
+			}
+		}
+		return flag;
+	}
+
+	public String[] fizzArray2(int n) {
+		String[] strArr = new String[n];
+		for (int i = 0; i < strArr.length; i++) {
+			strArr[i] = String.valueOf(i);
+		}
+		return strArr;
+	}
+
+	public boolean isEverywhere(int[] nums, int val) {
+
+		if (nums.length < 2) {
+			return true;
+		}
+
+		boolean flag = false;
+		for (int i = 0; i < nums.length - 1; i++) {
+
+			if (nums[i] == val || nums[i + 1] == val) {
+				flag = true;
+			} else {
+				return false;
+			}
+
+		}
+		return flag;
+	}
+
+	public boolean either24(int[] nums) {
+		if (nums.length < 2) {
+			return false;
+		}
+		boolean _22 = false;
+		boolean _44 = false;
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == 2 && nums[i + 1] == 2) {
+				_22 = true;
+			} else if (nums[i] == 4 && nums[i + 1] == 4) {
+				_44 = true;
+			}
+		}
+
+		if (_22 == _44) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public int matchUp(int[] nums1, int[] nums2) {
+		int count = 0;
+		for (int i = 0; i < nums2.length; i++) {
+			int n1 = Math.abs(nums1[i]);
+			int n2 = Math.abs(nums2[i]);
+			if ((n1 != n2 && (Math.abs(n1 - n2) <= 2))) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+}
--- src/hackerrank_easy/SolveMeFirst.java
+++ src/hackerrank_easy/SolveMeFirst.java
@@ -5,5 +5,4 @@ public class SolveMeFirst {
 	static int solveMeFirst(int a, int b) {
 		return a + b;
 	}
-
 }

64235fe523b8e533eb66a695ee4dea57fd503141
Omonjon Yokubov <yokubov.omonjon@gmail.com>
2020-10-24 16:29:05 -0400

    countClumps solution

--- src/codingbat/CodingBat_Arrays.java
+++ src/codingbat/CodingBat_Arrays.java
@@ -571,4 +571,18 @@ public class CodingBat_Arrays {
 		return max;
 	}
 
+	public static int countClumps(int[] nums) {
+		boolean match = false;
+		int count = 0;
+		for (int i = 0; i < nums.length-1; i++) {
+			if (nums[i] == nums[i+1] && !match) {
+				match = true;
+				count++;
+			}
+			else if (nums[i] != nums[i+1] ) {
+				match = false;
+			}
+		}
+		return count;
+	}
 }

5684ede4a402eab819c402e30617c356f2614f15
Omonjon Yokubov <yokubov.omonjon@gmail.com>
2020-10-26 00:42:16 -0400

    userCompare solution

--- src/ap_1/Ap_1.java
+++ src/ap_1/Ap_1.java
@@ -227,7 +227,28 @@ public class Ap_1 {
 		return count;
 	}
 
-	public static void main(String[] args) {
-		System.out.println(bigHeights(new int[] { 5, 13, 6, 7, 2 }, 0, 4));
+	public static int userCompare(String aName, int aId, String bName, int bId) {
+		
+		int nameOrder = aName.compareTo(bName);
+		
+		if (nameOrder > 0) {
+			return 1;
+		}
+		else if (nameOrder < 0) {
+			return -1;
+		}
+		
+		else if (nameOrder == 0) {
+			if (aId < bId) {
+				return -1;
+			}
+			else if (aId > bId) {
+				return 1;
+			}else {
+				return 1;
+			}
+		}
+		
+		return 0;
 	}
 }

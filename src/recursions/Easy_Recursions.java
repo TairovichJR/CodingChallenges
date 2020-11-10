b69c6da4d7565946308a3be1707032375290287b
TairovichJR <yokubov.omonjon@gmail.com>
2020-11-10 02:12:21 -0500

    replace pi solution

--- src/recursions/Easy_Recursions.java
+++ src/recursions/Easy_Recursions.java
@@ -111,4 +111,26 @@ public class Easy_Recursions {
               return 0 + countHi(str.substring(1));
       }
     }
+    public static String changeXY(String str) {
+
+        if (str.length() < 1){
+            return "";
+        }else if (str.charAt(0) == 'x'){
+            return 'y' + changeXY(str.substring(1));
+        }
+        return str.charAt(0) + changeXY(str.substring(1));
+    }
+
+    public static String changePi(String str) {
+        if (str.length() < 2){
+            return str;
+        }else{
+            if (str.startsWith("pi")){
+                return "3.14" + changePi(str.substring(2));
+            }else{
+                return str.charAt(0) +  changePi(str.substring(1));
+            }
+        }
+    }
+
 }

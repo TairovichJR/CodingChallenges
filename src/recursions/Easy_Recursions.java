8c4a022795ac79ff5eb133e0bc3f3cbbb23561c1
TairovichJR <yokubov.omonjon@gmail.com>
2020-11-10 01:05:12 -0500

    countX solution

--- src/recursions/Easy_Recursions.java
+++ src/recursions/Easy_Recursions.java
@@ -84,4 +84,21 @@ public class Easy_Recursions {
         }
         return base * powerN(base, n-1);
     }
+
+    public static int countX(String str) {
+
+        if (str.equals("")){
+            return 0;
+        }else{
+            char leftMostChar = str.charAt(0);
+            str = str.substring(1);
+            if (leftMostChar == 'x'){
+                return 1 + countX(str);
+            }else{
+                return 0 + countX(str);
+            }
+        }
+    }
+
+
 }

6ea7065ce55c6acbf4eba068a3f9a89af348c601
TairovichJR <yokubov.omonjon@gmail.com>
2021-02-26 17:04:10 -0500

    groupTotals

--- src/codingbat/CodingBat_Logical.java
+++ src/codingbat/CodingBat_Logical.java
@@ -1,5 +1,9 @@
 package codingbat;
 
+import java.lang.invoke.VarHandle;
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * Created by tairovich_jr on Oct 14, 2020
  */
@@ -27,7 +31,33 @@ public class CodingBat_Logical {
 		}
 	}
 
+	public static String[] groupTotals(String[] strArr){
+
+		Map<String,Integer> map = new HashMap<>();
+		for (int i = 0; i < strArr.length; i++) {
+			String[] pair = strArr[i].trim().split(":");
+			String key = pair[0].trim();
+			Integer value = Integer.parseInt(pair[1].trim());
+			if (map.containsKey(key)){
+				Integer newVal = map.get(key) + value;
+				map.put(key, newVal);
+			}else{
+				map.put(key,value);
+			}
+		}
+		List<String> list = new ArrayList<>();
+		map.entrySet().stream().filter(entry -> !entry.getValue().equals(0))
+		.forEach( entry -> {
+			String group = entry.getKey() + ":" + entry.getValue();
+			list.add(  group );
+		});
+		return list.stream().toArray(String[]::new);
+	}
+
+
 	public static void main(String[] args) {
 
+		System.out.println(Arrays.toString(groupTotals(new String[]{"X:-1", "Y:1", "X:-4", "B:3", "X:5"})));
+		System.out.println(Arrays.toString(groupTotals(new String[]{"B:-1", "A:1", "B:3", "A:5"})));
 	}
 }

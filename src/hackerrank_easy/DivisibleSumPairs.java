ff52da3595002f318c2f06a4542598a02271a467
TairovichJR <yokubov.omonjon@gmail.com>
2021-04-09 13:32:45 -0400

    divisibleSumPairs

---
+++ src/hackerrank_easy/DivisibleSumPairs.java
@@ -0,0 +1,26 @@
+package hackerrank_easy;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class DivisibleSumPairs {
+
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        int count = 0;
+        for (int i = 0; i < ar.length; i++) {
+            for (int j = i+1; j < ar.length; j++ ){
+                if ((ar[i] + ar[j]) % k == 0  ){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(divisibleSumPairs(6,3, new int[]{1,3,2,6,1,2}));
+    }
+
+}

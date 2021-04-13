3e9e1829dd2ed7e61022ee1a4aac408747678f9d
TairovichJR <yokubov.omonjon@gmail.com>
2021-04-13 02:24:16 -0400

    sockMerchant optimal solution with streams

--- src/hackerrank_easy/SalesByMatch.java
+++ src/hackerrank_easy/SalesByMatch.java
@@ -1,34 +1,26 @@
 package hackerrank_easy;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+
 
 public class SalesByMatch {
 
     static int sockMerchant(int n, int[] ar) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int count = 0;
-        for (int i = 0; i < ar.length; i++){
-            int num = ar[i];
-            if (map.containsKey(num)){
-                int value = map.get(num);
-                map.put(num, value+1);
-                value = map.get(num);
-                if (value % 2 == 0){
-                    count++;
-                    map.remove(num);
-                }
-            }else{
-                map.put(num, 1);
-            }
-        }
-        return count;
+
+       return Arrays.stream(ar)
+               .distinct()
+               .map(d -> Math.toIntExact(Arrays
+                       .stream(ar)
+                       .filter(i -> i == d)
+                       .count()))
+               .filter(i -> i != 1)
+               .map(i -> i / 2).sum();
     }
 
     public static void main(String[] args) {
 
-        System.out.println(sockMerchant(9, new int[]{10,20, 20, 10, 10, 30, 50, 10, 20}));
+        System.out.println(sockMerchant(10, new int[]{10, 20, 20, 10, 10, 30, 50, 10, 20}));
+
     }
 
 }

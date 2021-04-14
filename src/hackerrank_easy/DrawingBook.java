7798afadf7e17ce9bece3c435b4abacba3204456
TairovichJR <yokubov.omonjon@gmail.com>
2021-04-14 19:41:44 -0400

    pageCount bad solution

---
+++ src/hackerrank_easy/DrawingBook.java
@@ -0,0 +1,43 @@
+package hackerrank_easy;
+
+public class DrawingBook {
+
+
+    static int pageCount(int n, int p) {
+
+        int forwardFlip = 0;
+        int firstPage;
+        if (p % 2 != 0){
+            firstPage = 1;
+        }else{
+            firstPage = 0;
+        }
+        while (firstPage!= p && firstPage <= n){
+            firstPage += 2;
+            forwardFlip++;
+        }
+
+        int backwardFlip = 0;
+        int lastPage = n;
+        if ( p % 2 != 0){
+            if (lastPage % 2 == 0) {
+                lastPage += 1;
+            }
+        }else{
+            if (lastPage % 2 != 0) {
+                lastPage -= 1;
+            }
+        }
+        while (lastPage != p){
+            lastPage -= 2;
+            backwardFlip++;
+        }
+        return Math.min(forwardFlip, backwardFlip);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(pageCount(5,3));
+
+    }
+}
